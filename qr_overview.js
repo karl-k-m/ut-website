3e5bf0912d78715690a706b61b681bc30d19e5bf
Marko Tsengov <marko.tsengov@gmail.com>
2022-12-04 22:53:22 +0200

    Animatsioon

--- qr_overview.js
+++ qr_overview.js
@@ -1,4 +1,54 @@
 
+// Allikas: https://stackoverflow.com/a/39914235
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// animatsioon
+// Kood: Marko Tsengov
+
+// käita lõpmatult animatsiooni
+async function animation_loop() {
+    // liigutatav element
+    const data = document.getElementById("animation_data");
+
+    // algul reset
+    data.style.left = "500px";
+
+    await sleep(1000);
+
+    data.innerText = "Päring";
+    data.classList.remove("important");
+    data.classList.remove("destroy");
+    
+    // liigu veebilehitseja "ette"
+    
+    data.style.left = "420px";
+    
+    await sleep(1500);
+
+    // liigu serverisse
+    data.style.left = "10px";
+
+    await sleep(1000);
+
+    // võta "tähtis" olek ja liigu veebilehitseja juurde
+    data.classList.add("important");
+    data.innerText = "Andmed";
+    data.style.left = "320px";
+
+    await sleep(1500);
+
+    // "hävita" andmed
+    data.classList.add("destroy");
+    
+    // taaskäivita
+    setTimeout(animation_loop, 2000);
+}
+
+// alusta animatsioon "kõrval"
+setTimeout(animation_loop, 100);
+
 // modaali loomine
 // Kood (põhi): ChatGPT
 

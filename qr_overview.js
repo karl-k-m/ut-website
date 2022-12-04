0da4b4196d542eff101766b320144e4d8189b07e
Marko Tsengov <marko.tsengov@gmail.com>
2022-12-04 21:10:17 +0200

    Modaal ja rant

---
+++ qr_overview.js
@@ -0,0 +1,15 @@
+
+// modaali loomine
+// Kood (põhi): ChatGPT
+
+// näita modaali
+function showModal(id) {
+    var modal = document.getElementById(id);
+    modal.classList.add("show");
+}
+
+// peida modaal
+function hideModal(id) {
+    var modal = document.getElementById(id);
+    modal.classList.remove("show");
+}
